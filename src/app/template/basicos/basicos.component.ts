87547576de49371c84fe842c339ac8fe58153303
Rafael Ortiz <rortizv@unicartagena.edu.co>
2021-05-09 21:23:23 -0500

    formularios App - template validations

---
+++ src/app/template/basicos/basicos.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-basicos',
+  templateUrl: './basicos.component.html',
+  styles: [
+  ]
+})
+export class BasicosComponent implements OnInit {
+
+  @ViewChild('miFormulario') miFormulario!: NgForm;
+
+  //Si queremos inicializar algún campo del formulario con algún valor
+  initForm = {
+    // producto: '',
+    // precio: 0,
+    // existencias: 0
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  nombreValido(): boolean {
+    return this.miFormulario?.controls.producto?.invalid 
+            && this.miFormulario?.controls.producto?.touched;
+  }
+
+  precioValido():boolean {
+    return this.miFormulario?.controls.precio?.touched
+            && this.miFormulario?.controls.precio?.value < 0;
+  }
+
+  guardar() {
+
+    this.miFormulario.resetForm({
+      // producto: '',
+      // precio: 0,
+      // existencias: 0
+    });
+  }
+
+}
---
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[];
+}
+
+interface Favorito {
+  id: number;
+  nombre: string;
+}
+
+
+
+@Component({
+  selector: 'app-dinamicos',
+  templateUrl: './dinamicos.component.html',
+  styles: [
+  ]
+})
+export class DinamicosComponent {
+
+  nuevoJuego: string = '';
+
+  persona: Persona = {
+    nombre: 'Rafael',
+    favoritos: [
+      { id: 1, nombre: 'God of War III' },
+      { id: 2, nombre: 'Ninja Gaiden Zigma II' }
+    ]
+  }
+
+  agregarJuego() {
+    const nuevoFavorito: Favorito = {
+      id: this.persona.favoritos.length + 1,
+      nombre: this.nuevoJuego
+    }
+
+    this.persona.favoritos.push({ ...nuevoFavorito });
+    this.nuevoJuego = '';
+  }
+
+  eliminar( index: number ) {
+    this.persona.favoritos.splice(index, 1);
+  }
+
+
+  guardar() {
+    console.log('formulario posteado');
+  }
+}
