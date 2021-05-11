3bbe219969de1f6127c4b9ba531d2da19d2ed91b
Rafael Ortiz <rortizv@unicartagena.edu.co>
2021-05-10 23:25:05 -0500

    Formularios reactivos

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -6,11 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BasicosComponent implements OnInit {
+export class BasicosComponent {
 
-  constructor() { }
+  // miFormulario: FormGroup = new FormGroup({
+  //   nombre     : new FormControl('Coffee 1 Lb'),
+  //   precio     : new FormControl(1500),
+  //   existencias: new FormControl(20)
+  // })
 
-  ngOnInit(): void {
+  miFormulario: FormGroup = this.formBuilder.group({
+    nombre: [ , [Validators.required, Validators.minLength(3)] ],
+    precio: [ , [Validators.required, Validators.min(0)] ],
+    existencias: [ , [Validators.required, Validators.min(1)] ]
+  })
+
+  constructor(private formBuilder: FormBuilder ) { }
+
+  campoEsValido( campo: string ) {
+    return this.miFormulario.controls[campo].errors 
+        && this.miFormulario.controls[campo].touched;
   }
 
 }
